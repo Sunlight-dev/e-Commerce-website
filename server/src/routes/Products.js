5af554af9ad859d168cf498409c910cd3e6f66ce
Sunlight-dev <rameshgiri0822@gmail.com>
2023-08-17 17:58:52 -0500

    se crea endpoint get product por nombre y id

--- server/src/routes/Products.js
+++ server/src/routes/Products.js
@@ -1,11 +1,13 @@
 const {Router} = require("express")
 const {getAllProducts} = require("../handlers/productsHandlers/allProducts")
 const { getProductsHandler } = require("../handlers/productsHandlers/getProductsByIdHandler")
+const { getProductByNameHandler } = require("../handlers/productsHandlers/getProductByNameHandler")
 
 const Products = Router()
 
 Products.get("/", getAllProducts)
 Products.get("/:id", getProductsHandler)
-console.log('hola mundo');
+Products.get('/name', getProductByNameHandler)
+
 
 module.exports = Products
