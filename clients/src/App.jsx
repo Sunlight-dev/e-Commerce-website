3c2d0b31699c447b5206be0768f30174a1058698
Sunlight-dev <rameshgiri0822@gmail.com>
2023-08-21 16:46:11 -0500

    se crea carpeta de create-product

--- clients/src/App.jsx
+++ clients/src/App.jsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import Start from "./pages/Home/Start";
+import Form from "./pages/CreateProduct/Form";
 
 export default function App() {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Start />} />
+        <Route path="/createproduct" element={<Form/>}/>
       </Routes>
     </div>
   );
