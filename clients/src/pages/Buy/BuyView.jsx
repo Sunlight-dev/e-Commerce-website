5c15d97589efe26f8a162d52dd2d8311a0a3dae9
Sunlight-dev <rameshgiri0822@gmail.com>
2023-08-21 18:37:39 -0300

    fixes

---
+++ clients/src/pages/Buy/BuyView.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Styles from './BuyView.module.jsx'
+import Card from '../../components/Card/Card.jsx'
+
+export default function BuyView() {
+  return (
+    <div>BuyView
+
+        <Card/>
+    </div>
+
+  )
+}
