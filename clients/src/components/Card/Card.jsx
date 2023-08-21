099144d80d29121391eacda316eb7234ea9dcd28
Sunlight-dev <rameshgiri0822@gmail.com>
2023-08-21 17:49:27 -0300

    start card

--- clients/src/components/Card/Card.jsx
+++ clients/src/components/Card/Card.jsx
@@ -3,7 +3,10 @@ import Styles from './Card.module.css'
 export default function Card() {
   return (
     <div className={Styles.wrapper}>
-
+        <div className="">
+            <p>{article.stock}</p>
+        </div>
+        <div className=""></div>
     </div>
   )
 }
