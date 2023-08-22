20d95adb84d81ad87403e53370552fd61a1a1cfe
Sunlight-dev <rameshgiri0822@gmail.com>
2023-08-22 12:04:07 -0500

    Paginado y relación a categoría al traer los productos

--- server/src/controllers/productsControllers/getAllProductsController.js
+++ server/src/controllers/productsControllers/getAllProductsController.js
@@ -1,11 +1,25 @@
-const {Product} = require("../../db")
+const {Product,Category} = require("../../db")
 
 const getAllProductsController= async (page,size)=>{
-    const products = await Product.findAndCountAll({
+    const productList = await Product.findAndCountAll({
         limit:size,
-        offset:page * size
-    });
-    return products
+        offset:page * size,
+        include: [
+            {
+              model: Category,
+              attributes: ['name'], // Selecciona solo el campo 'name' de la categoría
+            },
+          ],
+          attributes: { exclude: ['categoryId'] },
+        }
+        );
+    const products = productList.rows.map(product => ({
+        ...product.toJSON(),
+        categoria: product.Category.name, // Agrega el nombre de la categoría como propiedad 'categoria'
+        Category: undefined, // Elimina la propiedad Category si no es deseada
+        })
+    );
+    return products;
 }
 
-module.exports = getAllProductsController
+module.exports = getAllProductsController;
