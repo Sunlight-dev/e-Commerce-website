b2b4925b5dd484dc2d54e27dada97e8d058ae0ad
Sunlight-dev <rameshgiri0822@gmail.com>
2023-08-22 17:45:01 -0300

    Products page front

--- clients/src/pages/Buy/BuyView.jsx
+++ clients/src/pages/Buy/BuyView.jsx
@@ -3,10 +3,14 @@ import Styles from './BuyView.module.css'
 import Card from '../../components/Card/Card.jsx'
 import Categories from '../../components/Categories/Categories'
 import ListCard from '../../components/Card/ListCard'
+import Nav from '../../components/navBar/Nav'
 
 
 export default function BuyView() {
   return (
+    <div className="">
+      <Nav/>
+
     <div className={Styles.wrapper}>
       <div className={Styles.categories}>
         <Categories />
@@ -14,6 +18,7 @@ export default function BuyView() {
       <div className={Styles.listCard} >
         <ListCard />
       </ div>
+    </div>
     </div>
 
   )
