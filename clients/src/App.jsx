54a1e5059b6e0874d39d9c0cfd52963073682df5
Sunlight-dev <rameshgiri0822@gmail.com>
2023-08-22 01:01:03 -0400

    Cambios en importación

--- clients/src/App.jsx
+++ clients/src/App.jsx
@@ -1,14 +1,14 @@
-import { Route, Routes } from "react-router-dom";
-import Start from "./pages/Home/Start";
-import Form from "./pages/CreateProduct/Form";
+import { Route, Routes } from 'react-router-dom'
+import Start from './pages/Home/Start'
+import Form from './pages/CreateProduct/Form'
 
 export default function App() {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Start />} />
-        <Route path="/createproduct" element={<Form/>}/>
+        <Route path="/createproduct" element={<Form />} />
       </Routes>
     </div>
-  );
+  )
 }
