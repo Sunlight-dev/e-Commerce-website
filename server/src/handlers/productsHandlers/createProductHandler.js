6c997ee33644c89e98e1dcebe29c1a52e9236165
Sunlight-dev <rameshgiri0822@gmail.com>
2023-08-22 11:43:16 -0500

    Eliminasción de función que busca el id de categoria y exportación como objeto

--- server/src/handlers/productsHandlers/createProductHandler.js
+++ server/src/handlers/productsHandlers/createProductHandler.js
@@ -1,17 +1,15 @@
-const { createProductController } = require("../../controllers/productsControllers/createProductController")
+const  createProductController  = require("../../controllers/productsControllers/createProductController")
 const { Category }= require("../../db")
 
 const createProductHandler = async(req,res)=>{
     const {name,description,price,stock,valoration,image,category}= req.body;
 
-    const foundCategory = await category.findOne({where: {name: category}});
-    const categoryId= foundCategory ? foundCategory.id : null;
     try {
-        const result = await createProductController(name,description,price,stock,valoration,image,categoryId)
+        const result = await createProductController(name,description,price,stock,valoration,image,category)
         res.status(201).json(result)
     } catch (error) {
         res.status(400).json({error: error.message})
     }
 }
 
-module.exports={createProductHandler}
+module.exports = createProductHandler;
