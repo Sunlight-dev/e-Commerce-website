9b25957565d70d757918766e5ec5233e53698b8d
Sunlight-dev <rameshgiri0822@gmail.com>
2023-08-22 18:51:38 -0500

    Inclusión de ruta Categories

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,8 +1,10 @@
-const { Router } = require("express")
-const productsRouter = require("./productRoutes");
+const { Router } = require('express');
+const productsRouter = require('./productRoutes');
+const categoriesRouter = require('./categoriesRoutercategories');
 
 const router = Router();
 
-router.use("/products", productsRouter);
+router.use('/products', productsRouter);
+router.use('/categories', categoriesRouter);
 
 module.exports = router;
