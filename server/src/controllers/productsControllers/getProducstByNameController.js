788fda16e2852e9bcd08d06f84db70b86dddacf7
Sunlight-dev <rameshgiri0822@gmail.com>
2023-08-22 12:05:56 -0500

    Paginado y relación con categría la traer productos por nombre

--- server/src/controllers/productsControllers/getProducstByNameController.js
+++ server/src/controllers/productsControllers/getProducstByNameController.js
@@ -1,12 +1,26 @@
-const {Product} = require("../../db")
+const {Product,Category} = require("../../db");
+const {Op} = require('sequelize');
 
-  const getProductsByNameController = async (name)=>{
-    const productsByName = await Product.findAll(
-        {where:
-            {name:name}}
-        );
-
+  const getProductByNameController = async (name,page,size)=>{
+    const productList = await Product.findAndCountAll({
+            limit:size,
+            offset:page * size,
+            where:{name:{[Op.iLike]:`%${name}%`}},
+            include: [
+              {
+                model: Category,
+                attributes: ['name'], 
+              },
+            ],
+            attributes: { exclude: ['categoryId'] },
+          });
+    const productsByName = productList.rows.map(product => ({
+        ...product.toJSON(),
+        categoria: product.Category.name, 
+        Category: undefined, 
+        })
+    );
     return productsByName
 }
 
-module.exports =  getProductsByNameController
+module.exports =  getProductByNameController;
