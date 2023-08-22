b473dd28152f1757c04eb65e9e60a7792b3ce287
Sunlight-dev <rameshgiri0822@gmail.com>
2023-08-21 19:46:30 -0500

    modificación de getProductsHandler para paginado

--- server/src/handlers/productsHandlers/getProductsHandler.js
+++ server/src/handlers/productsHandlers/getProductsHandler.js
@@ -1,18 +1,29 @@
-
 const getAllProductsController = require ('../../controllers/productsControllers/getAllProductsController')
 const getProductByNameController = require ('../../controllers/productsControllers/getProductByNameController')
 
+const setPaginationData = (page,size) =>{
+    const data = {page:0,
+                  size:12};
+    const pageAsNumber = Number.parseInt(page);
+    const sizeAsNumber = Number.parseInt(size);
+    if (!Number.isNaN(pageAsNumber) && pageAsNumber > 0 ) 
+        data.page = pageAsNumber;
+    if (!Number.isNaN(sizeAsNumber) && sizeAsNumber > 0 && sizeAsNumber <=12)
+        data.size = sizeAsNumber;
+    return data;
+};
 
 const getProductsHandler = async(req,res)=>{
-    const { name }= req.query;
-    
+    const { name , page, size}= req.query;
+    const paginationData = setPaginationData(page,size);
+
     try {
         if(name){
-            const result = await getProductByNameController(name);
+            const result = await getProductByNameController(name,paginationData.page,paginationData.size);
             res.status(200).json(result)
         }
         else{
-            const result = await getAllProductsController();
+            const result = await getAllProductsController(paginationData.page,paginationData.size);
             res.status(200).json(result)
         }
     } catch (error) {
