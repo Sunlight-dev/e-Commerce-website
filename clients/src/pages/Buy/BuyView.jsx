7bebda1de2ea562d2a9ee1ac03513968838c21df
Sunlight-dev <rameshgiri0822@gmail.com>
2023-08-22 14:22:09 -0300

    ft - list categories

--- clients/src/pages/Buy/BuyView.jsx
+++ clients/src/pages/Buy/BuyView.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Styles from './BuyView.module.css'
 import Card from '../../components/Card/Card.jsx'
+import Categories from '../../components/Categories/Categories'
 
 
 export default function BuyView() {
   return (
     <div>
-        <Card/>
+
+      <Categories/>
     </div>
 
   )
