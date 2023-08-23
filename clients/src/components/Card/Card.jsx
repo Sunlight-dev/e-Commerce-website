c888f21932870a2552ef561c1580078c980497e7
Sunlight-dev <rameshgiri0822@gmail.com>
2023-08-23 02:40:11 -0300

    aviability redux

--- clients/src/components/Card/Card.jsx
+++ clients/src/components/Card/Card.jsx
@@ -1,30 +1,32 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import Styles from './Card.module.css'
 import { AiOutlineHeart, AiFillPlusCircle, AiOutlineMinusCircle } from "react-icons/ai";
 
 
 export default function Card(props) {
+    let imgCard = "https://static.skyassets.com/contentstack/assets/blt143e20b03d72047e/bltbae4788745bf0010/6319d665937b9657b5f2b7ee/Carousel_iPhone14ProMax_Purple_Placement02-PreOrder.png"
+    
     let [buyQ, setBuyQ] = useState(1)
     let handleMoreQ = ()=>{
         setBuyQ(buyQ + 1)
     }
     let handleLessQ = ()=>{
         if(buyQ > 0){
-
+            
             setBuyQ(buyQ - 1)
         }
         else setBuyQ(buyQ)
     }
     
     
-  return (
-    <div className={Styles.wrapper}>
+    return (
+        <div className={Styles.wrapper}>
         <div className={Styles.div_image}>
             <div className={Styles.stock}>
-                <p>Abailable</p>
+                <p>{props.aviability ? 'Avalable' : 'Unavailable'}</p>
                 <AiOutlineHeart/>
             </div>
-            <img className={Styles.img_product} src="https://static.skyassets.com/contentstack/assets/blt143e20b03d72047e/bltbae4788745bf0010/6319d665937b9657b5f2b7ee/Carousel_iPhone14ProMax_Purple_Placement02-PreOrder.png" alt="" />
+            <img className={Styles.img_product} src={ imgCard } alt="" />
 
         </div>
         <div className={Styles.description_card}>
