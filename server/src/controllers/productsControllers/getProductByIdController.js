f63e22feded3e1c071573a4531d248e8b233b59b
Sunlight-dev <rameshgiri0822@gmail.com>
2023-08-23 07:51:33 -0500

    Corrección en la devolución de propiedad Categoria por category en getProduc by Name y by Id

--- server/src/controllers/productsControllers/getProductByIdController.js
+++ server/src/controllers/productsControllers/getProductByIdController.js
@@ -1,15 +1,23 @@
 const {Product, Category} = require('../../db')
 
-
-
 const getProductByIdController = async (id) => {
-  const product  = await Product.findByPk( id, {
-    include: {
-      model: Category,
-      attributes: ["name"], 
-    },
-  });
-    return product;
+  const productFound  = await Product.findByPk( id, {
+    include: [
+      {
+        model: Category,
+        attributes: ['name'], 
+      },
+    ],
+    attributes: { exclude: ['categoryId'] },
+  }
+  );
+
+  const product = {...productFound.toJSON(),
+                  category: productFound.Category.name, 
+                  Category: undefined, // Elimina la propiedad Category si no es deseada
+    }
+
+  return product;
   };
 
   module.exports = getProductByIdController
