ccf6b0d384554a59a38f1b45616130c97c355f17
Sunlight-dev <rameshgiri0822@gmail.com>
2023-08-24 11:54:43 -0500

    Implementación de ruta deleteUser, handler y controller

--- server/src/routes/usersRouter.js
+++ server/src/routes/usersRouter.js
@@ -3,7 +3,7 @@ const getUsersHandler = require('../handlers/usersHandlers/getUsersHandler.js');
 const getUserByIdHandler = require('../handlers/usersHandlers/getUserByIdHandler.js');
 const createUserHandler = require('../handlers/usersHandlers/createUserHandler.js');
 const updateUserHandler = require('../handlers/usersHandlers/updateUserHandler.js');
-// const deleteUserHandler = require('../handlers/usersHandlers/deleteUserHandler.js');
+const deleteUserHandler = require('../handlers/usersHandlers/deleteUserHandler.js');
 
 const userRouter = Router();
 
@@ -11,6 +11,6 @@ userRouter.get("/", getUsersHandler);
 userRouter.get("/:id", getUserByIdHandler);
 userRouter.post("/", createUserHandler);
 userRouter.put("/:id", updateUserHandler);
-// userRouter.delete("/:id", deleteUserHandler);
+userRouter.delete("/:id", deleteUserHandler);
 
 module.exports = userRouter;
