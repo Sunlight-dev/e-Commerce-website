178d60eaaeef0d59bbb63e59353f38644d1d7a39
Sunlight-dev <rameshgiri0822@gmail.com>
2023-08-23 23:08:22 -0500

    Implementación de ruta updateCategory, handler y controller

--- server/src/routes/categoriesRouter.js
+++ server/src/routes/categoriesRouter.js
@@ -1,10 +1,13 @@
 const {Router} = require('express');
 const addCategoryHandler = require('../handlers/categoriesHandlers/createCategoryHandler.js');
 const getAllCategoriesHandler = require('../handlers/categoriesHandlers/getAllCategoriesHandler.js');
+const updateCategoryHandler = require('../handlers/categoriesHandlers/updateCategoryHandler.js');
 
 const categoryRouter = Router();
 
 categoryRouter.get('/',getAllCategoriesHandler);
 categoryRouter.post('/',addCategoryHandler);
+categoryRouter.put('/:id',updateCategoryHandler);
+
 
 module.exports = categoryRouter;
