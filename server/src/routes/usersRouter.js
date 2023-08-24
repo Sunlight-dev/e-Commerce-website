5fd11c0c53ef67af43761c56f6f5d3e98368da8e
Sunlight-dev <rameshgiri0822@gmail.com>
2023-08-24 11:35:57 -0500

    Implementación de ruta updateUser, handler y controller

--- server/src/routes/usersRouter.js
+++ server/src/routes/usersRouter.js
@@ -2,7 +2,7 @@ const {Router} = require('express');
 const getUsersHandler = require('../handlers/usersHandlers/getUsersHandler.js');
 const getUserByIdHandler = require('../handlers/usersHandlers/getUserByIdHandler.js');
 const createUserHandler = require('../handlers/usersHandlers/createUserHandler.js');
-// const updateUserHandler = require('../handlers/usersHandlers/updateUserHandler.js');
+const updateUserHandler = require('../handlers/usersHandlers/updateUserHandler.js');
 // const deleteUserHandler = require('../handlers/usersHandlers/deleteUserHandler.js');
 
 const userRouter = Router();
@@ -10,7 +10,7 @@ const userRouter = Router();
 userRouter.get("/", getUsersHandler);
 userRouter.get("/:id", getUserByIdHandler);
 userRouter.post("/", createUserHandler);
-// userRouter.put("/:id", updateUserHandler);
+userRouter.put("/:id", updateUserHandler);
 // userRouter.delete("/:id", deleteUserHandler);
 
 module.exports = userRouter;
