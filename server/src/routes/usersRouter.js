4958a9c9ee3128ee7d8c205eb575f1eb30a1e296
Sunlight-dev <rameshgiri0822@gmail.com>
2023-08-24 11:03:01 -0500

    Implementación de ruta getUsersById, handler y controller

--- server/src/routes/usersRouter.js
+++ server/src/routes/usersRouter.js
@@ -1,6 +1,6 @@
 const {Router} = require('express');
 const getUsersHandler = require('../handlers/usersHandlers/getUsersHandler.js');
-// const getUserByIdHandler = require('../handlers/usersHandlers/getUserByIdHandler.js');
+const getUserByIdHandler = require('../handlers/usersHandlers/getUserByIdHandler.js');
 const createUserHandler = require('../handlers/usersHandlers/createUserHandler.js');
 // const updateUserHandler = require('../handlers/usersHandlers/updateUserHandler.js');
 // const deleteUserHandler = require('../handlers/usersHandlers/deleteUserHandler.js');
@@ -8,7 +8,7 @@ const createUserHandler = require('../handlers/usersHandlers/createUserHandler.j
 const userRouter = Router();
 
 userRouter.get("/", getUsersHandler);
-// userRouter.get("/:id", getUserByIdHandler);
+userRouter.get("/:id", getUserByIdHandler);
 userRouter.post("/", createUserHandler);
 // userRouter.put("/:id", updateUserHandler);
 // userRouter.delete("/:id", deleteUserHandler);
