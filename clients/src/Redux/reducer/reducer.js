16c3246f72f9fdd8765f8cc2355bea7f460e341c
Sunlight-dev <rameshgiri0822@gmail.com>
2023-08-26 14:29:58 -0400

    Cambios en la card

--- clients/src/Redux/reducer/reducer.js
+++ clients/src/Redux/reducer/reducer.js
@@ -1,83 +1,89 @@
-import { POST_PDT, GET_PDT, GET_DET, GET_CATEGORIES, FILTER_BY_CATEGORY, ORDER_BY_PRICE, ORDER_BY_VALORATION } from "../actions/actionTypes";
+import {
+  POST_PDT,
+  GET_PDT,
+  GET_DET,
+  GET_CATEGORIES,
+  FILTER_BY_CATEGORY,
+  ORDER_BY_PRICE,
+  ORDER_BY_VALORATION,
+} from '../actions/actionTypes'
 
 const initialState = {
-      base: [],
-      products: [],
-      allProducts: [],
-      categories:[],
-      detail:[]
-  };
-  
-  const rootReducer = (state = initialState, action) => {
-    switch (action.type) {
-      case GET_PDT:
-        return{
-          ...state,
-            products: action.payload,
-            allProducts: action.payload
-        }
+  base: [],
+  products: [],
+  allProducts: [],
+  categories: [],
+  detail: [],
+}
 
-      case GET_DET:
-        return{
-          ...state,
-          detail: action.payload
-        }
-      case POST_PDT:
-        return {
-          ...state
-        }
+const rootReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case GET_PDT:
+      return {
+        ...state,
+        products: action.payload,
+        allProducts: action.payload,
+      }
 
-        case GET_CATEGORIES:
-            return {
-                ...state,
-                categories: action.payload
-            }
+    case GET_DET:
+      return {
+        ...state,
+        detail: action.payload,
+      }
+    case POST_PDT:
+      return {
+        ...state,
+      }
 
-        case FILTER_BY_CATEGORY:
-          const allProductsCat = state.allProducts;
-          const filteredCategory = action.payload === 'All'? allProductsCat : allProductsCat.filter(v =>v.category == action.payload);
-          return {
-              ...state,
-              products: filteredCategory
-          }
-      
-      case ORDER_BY_PRICE:
-          let orderPriceAsc = state.products.slice().sort((a, b) => {
+    case GET_CATEGORIES:
+      return {
+        ...state,
+        categories: action.payload,
+      }
 
-              if(Number(a.price) > Number(b.price)) return 1;
-
-              if(Number(b.price) > Number(a.price)) return -1;
-
-              return 0;
-          })
-
-          return {
-              ...state,
-              products: action.payload === 'asc' ? orderPriceAsc : orderPriceAsc.reverse()
-          }
-
-        
-    case ORDER_BY_VALORATION:
-
-        let orderValorationAsc = state.products.slice().sort((a, b) => {
-
-            if(Number(a.valoration) > Number(b.valoration)) return 1;
-          
-            if(Number(b.valoration) > Number(a.valoration)) return -1;
-          
-            return 0;
-        })
-        
-        return {
-            ...state,
-            products: orderValorationAsc
+    case FILTER_BY_CATEGORY: {
+      const allProductsCat = state.allProducts
+      const filteredCategory =
+        action.payload === 'All'
+          ? allProductsCat
+          : allProductsCat.filter((v) => v.category === action.payload)
+      return {
+        ...state,
+        products: filteredCategory,
+      }
     }
 
-  
-      default:
-        return state;
+    case ORDER_BY_PRICE: {
+      const orderPriceAsc = [...state.products].sort((a, b) => {
+        if (Number(a.price) > Number(b.price)) return 1
+        if (Number(b.price) > Number(a.price)) return -1
+        return 0
+      })
+
+      return {
+        ...state,
+        products:
+          action.payload === 'asc' ? orderPriceAsc : orderPriceAsc.reverse(),
+      }
     }
-  };
-  
-  export default rootReducer;
-  
+
+    case ORDER_BY_VALORATION: {
+      let orderValorationAsc = state.products.slice().sort((a, b) => {
+        if (Number(a.valoration) > Number(b.valoration)) return 1
+
+        if (Number(b.valoration) > Number(a.valoration)) return -1
+
+        return 0
+      })
+
+      return {
+        ...state,
+        products: orderValorationAsc,
+      }
+    }
+    default:
+      return state
+  }
+}
+
+export default rootReducer
