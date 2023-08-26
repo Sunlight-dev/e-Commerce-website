c5165fa9a8512f0d9fbbcbef9d25b6180b717224
Sunlight-dev <rameshgiri0822@gmail.com>
2023-08-26 07:14:46 -0500

    ruta putProductReviewsByUser con handler y controller

--- server/src/routes/reviewsRouter.js
+++ server/src/routes/reviewsRouter.js
@@ -1,13 +1,13 @@
 const {Router} = require('express');
 const getProductReviewsHandler = require ('../handlers/reviewsHandlers/getProductReviewsHandler.js');
 const postProductReviewByUserHandler = require ('../handlers/reviewsHandlers/postProductReviewByUserhandler.js');
-
+const updateProductReviewByUserHandler = require ('../handlers/reviewsHandlers/updateProductReviewByUserHandler.js');
 
 const reviewRouter = Router();
 
 reviewRouter.get('/:productId',getProductReviewsHandler);//id corresponde a producto
 reviewRouter.post('/:productId',postProductReviewByUserHandler);//id de user pasado por query
-// reviewRouter.put('/:id',updateProductReviewByUserHandler);//id de user pasado por query
+reviewRouter.put('/:productId',updateProductReviewByUserHandler);//id de user pasado por query
 // reviewRouter.delete('/:id',getProductReviewByUserHandler);//id de user pasado por query
 
 module.exports = reviewRouter;
