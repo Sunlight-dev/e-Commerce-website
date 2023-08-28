df4fc8b145cd9bd4bf95c7dd52c7dc8ff009409d
Sunlight-dev <rameshgiri0822@gmail.com>
2023-08-28 12:14:45 -0300

    searchBar

--- clients/src/components/searchBar/searchBar.jsx
+++ clients/src/components/searchBar/searchBar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useState } from "react";
 import { useDispatch } from "react-redux";
-import { getNameProducts } from "../../Redux/actions/actions";
+import getNameProducts from "../../Redux/actions/actions"
 import s from "./SearchBar.module.css"
 
 export default function SearchBar () {
