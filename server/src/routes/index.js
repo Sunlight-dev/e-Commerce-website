09a11b3a8009ef7cdb00f962e8c35eac7f173603
Sunlight-dev <rameshgiri0822@gmail.com>
2023-08-29 13:49:41 -0500

    creación de ruta brands handler y controller

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,8 +1,9 @@
 const { Router } = require('express');
-const productsRouter = require('./productRoutes');
-const categoriesRouter = require('./categoriesRouter');
+const productsRouter = require('./productRoutes.js');
+const categoriesRouter = require('./categoriesRouter.js');
 const usersRouter = require('./usersRouter.js');
-const reviewsRouter = require('./reviewsRouter');
+const reviewsRouter = require('./reviewsRouter.js');
+const brandsRouter = require('./brandsRouter.js');
 
 const router = Router();
 
@@ -10,5 +11,6 @@ router.use('/products', productsRouter);
 router.use('/categories', categoriesRouter);
 router.use('/users', usersRouter);
 router.use('/reviews', reviewsRouter);
+router.use('/brands', brandsRouter);
 
 module.exports = router;
