badb6a468887552184ff893f30aa98ac1475b4c1
Sunlight-dev <rameshgiri0822@gmail.com>
2023-09-04 07:01:39 -0500

    se incluye paginado en ruta getAllOrders handler y controller

--- server/src/handlers/ordersHandler/getAllOrdersHandler.js
+++ server/src/handlers/ordersHandler/getAllOrdersHandler.js
@@ -1,9 +1,24 @@
 const getAllOrdersController = require('../../controllers/ordersControllers/getAllOrdersController.js');
 
+const setPaginationData = (page,size) =>{
+    const data = {page:page,
+                  size:size};
+    const pageAsNumber = Number.parseInt(page);
+    const sizeAsNumber = Number.parseInt(size);
+    if (!Number.isNaN(pageAsNumber) && pageAsNumber > 0 ) 
+        data.page = pageAsNumber;
+    else data.page = 0;
+    if (!Number.isNaN(sizeAsNumber) && sizeAsNumber > 0 && sizeAsNumber <=12)
+        data.size = sizeAsNumber;
+    else data.size = 12;
+    return data;
+};
+
 const getAllOrdersHandler = async(req,res)=>{
-    const {status,userId} = req.query;
+    const {status,userId,page,size} = req.query;
+    const paginationData = setPaginationData(page,size);
     try {
-        const result = await getAllOrdersController(status,userId);
+        const result = await getAllOrdersController(status,userId,paginationData.page,paginationData.size);
         res.status(200).json(result);
     } 
     catch (error) {
