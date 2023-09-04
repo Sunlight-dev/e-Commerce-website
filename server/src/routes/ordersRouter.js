f3e5d6367be6952ca02a3dda1712194823f7d250
Sunlight-dev <rameshgiri0822@gmail.com>
2023-09-04 07:18:25 -0500

    ruta getOrderById con handler y controller

--- server/src/routes/ordersRouter.js
+++ server/src/routes/ordersRouter.js
@@ -1,7 +1,6 @@
 const {Router} = require('express');
 const getAllOrdersHandler = require('../handlers/ordersHandler/getAllOrdersHandler.js');
-const getOrderByIdHandler = require('../handlers/ordersHandler/getAllOrdersHandler.js');
-const getOrdersByUserHandler = require('../handlers/ordersHandler/getOrdersByUserHandler.js');
+const getOrderByIdHandler = require('../handlers/ordersHandler/getOrderByIdHandler.js');
 const createOrderHandler = require('../handlers/ordersHandler/createOrderHandler.js');
 const putStateOrderHandler = require('../handlers/ordersHandler/putStateOrderHandler.js');
 const deleteOrderHandler = require('../handlers/ordersHandler/deleteOrderHandler.js');
@@ -10,7 +9,6 @@ const ordersRouter = Router();
 
 ordersRouter.get('/',getAllOrdersHandler); 
 ordersRouter.get('/:id',getOrderByIdHandler); 
-// ordersRouter.get('/:userId',getOrdersByUserHandler); 
 ordersRouter.post('/:userId',createOrderHandler);
 // ordersRouter.put('/:id',putStateOrderHandler);
 // ordersRouter.delete('/:id',deleteOrderHandler); 
