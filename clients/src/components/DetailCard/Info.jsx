1c864751dcb0ea3422e37f82502056a7838bae0b
Sunlight-dev <rameshgiri0822@gmail.com>
2023-09-04 20:43:10 -0600

    se añade valoracion y casi terminado reviews

--- clients/src/components/DetailCard/Info.jsx
+++ clients/src/components/DetailCard/Info.jsx
@@ -1,12 +1,14 @@
 import Styles from './Info.module.css'
+import Rating from '../Rating/Rating'
 
 // eslint-disable-next-line react/prop-types
-export default function Info({name, description,stock,price}) {
+export default function Info({name, description, price, valoration, stock,}) {
   return (
     <div className={Styles.wrapper}>
       <h3 className={Styles.pdt_name}>{name} </h3>
       <p className={Styles.pdt_description}>{description}</p>
-      <p className={Styles.pdt_stock}>stock: {stock}</p>
+      <div><Rating valoration={valoration}></Rating></div>
+      <p className={Styles.pdt_stock}>Stock: {stock}</p>
       <p className={Styles.pdt_price}>${price}</p>
       <button className={Styles.btn_buy}>Buy now</button>
     </div>
