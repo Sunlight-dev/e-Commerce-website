7079bed35862f46487ac24d0a9c28578e7d401b0
Sunlight-dev <rameshgiri0822@gmail.com>
2023-09-05 20:08:23 -0300

    nav-cart

---
+++ clients/src/components/ShoppingCar/NavCart.jsx
@@ -0,0 +1,20 @@
+import styles from "./ShoppingCart.module.css";
+import logo from "../../images/logo.png";
+import { Link } from "react-router-dom";
+import { IoArrowBackOutline } from "react-icons/io5";
+
+export const NavCart = () => {
+  return (
+    <div className={styles.container_nav}>
+      <div className={styles.orderTitle}>
+      <Link to="/products">
+          <IoArrowBackOutline className={styles.backIcon}/>
+        </Link>        
+        <h1>Your order</h1>
+        </div>
+        <div>
+          <img src={logo} alt="logo" className={`${styles.logo}`} />
+        </div>
+    </div>
+  );
+};
