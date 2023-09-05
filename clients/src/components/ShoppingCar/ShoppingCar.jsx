f52238421066e5723332109f2e8f39208e85d185
Sunlight-dev <rameshgiri0822@gmail.com>
2023-09-04 23:00:34 -0300

    Arreglos css cart

--- clients/src/components/ShoppingCar/ShoppingCar.jsx
+++ clients/src/components/ShoppingCar/ShoppingCar.jsx
@@ -1,75 +1,65 @@
-import { useDispatch } from 'react-redux'
-import { createOrder } from '../../Redux/actions/actions'
-import { useSelector } from 'react-redux'
-
-
+import styles from "./ShoppingCart.module.css";
+import { useDispatch } from "react-redux";
+import { createOrder } from "../../Redux/actions/actions";
+import { useSelector } from "react-redux";
+import { FaDeleteLeft } from "react-icons/fa6";
 
 export default function ShoppingCar() {
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    const shoppingCart = useSelector((state) => state.cart)
+  const shoppingCart = useSelector((state) => state.cart);
+  console.log(shoppingCart);
 
-
-    let shoppingCart2 = shoppingCart.map((item) => {
-        return {
-            title: item.name,
-            unit_price: item.price,
-            current_id: "ARS",
-            quantity: item.buyQ,
-        };
-    });
-
-    // const shoppingCart22 = [
-    //     {
-    //         current_id: "ARS",
-    //         quantity: 1,
-    //         unit_price: 100,
-    //         title: "Laptop"
-    //     },
-    //     {
-    //         current_id: "ARS",
-    //         quantity: 1,
-    //         unit_price: 200,
-    //         title: "a"
-    //     }
-    // ]
-  
-
-    const handleBuy = async () => {
-       
-        
-        dispatch(createOrder(shoppingCart2))
-        // dispatch(createOrder(shoppingCart));
+  // Agrupa los productos por id y suma las cantidades
+  const groupedCart = shoppingCart.reduce((acc, item) => {
+    if (!acc[item.id]) {
+      acc[item.id] = { ...item, quantity: 0 };
     }
+    acc[item.id].quantity += item.buyQ;
+    return acc;
+  }, {});
 
-    return (
-        // En este componente se renderiza el carrito de compras, en el cual tal vez se muestren los productos y un boton de comprar, dicho boton de comprar deberia redirigir a la pagina de mercadopago, donde se realizara la compra
-        <div>
-            {
-                shoppingCart.map((item) => {
-                    return (
-                        <div key={item.id}>
+  const shoppingCart2 = Object.values(groupedCart).map((item) => ({
+    title: item.name,
+    unit_price: item.price,
+    current_id: "ARS",
+    quantity: item.quantity,
+  }));
 
-                            <h3>{item.name}</h3>
-                            <h3>{item.price}</h3>
-                            <h3>{item.buyQ}</h3>
-                            <br />
-                        </div>
-                    )
-                }
-                )
+  const handleBuy = async () => {
+    dispatch(createOrder(shoppingCart2));
+    // dispatch(createOrder(shoppingCart));
+  };
 
-            }
+  return (
+    <div className={styles.container_shoppingCart}>
+      {Object.values(groupedCart).map((item, index) => (
+        <div key={item.id} className={styles.cart_products}>
+          <h4>
+            <b>Order #{index + 1}</b>{" "}
+          </h4>
+          <h6>
+            <b>Product name: </b> {item.name}
+          </h6>
+          <h6>
+            <b> Price: </b> {item.price}
+          </h6>
+          <div className={styles.container_quantity}> 
 
-            <div className='card'>
-                {/* <h3>Productos</h3> */}
-                {/* <p className='price'>100 $</p> */}
-
-                <button onClick={handleBuy}>Buy</button>
-
-            </div>
+          <h6>
+            <b>Quantity: </b>
+            {item.quantity}
+          </h6>
+          <button className={styles.btn_delete}>
+            <FaDeleteLeft className={styles.btn}/>
+          </button>
+          </div>
         </div>
+      ))}
 
-    )
+      <div className={styles.buy_container}>
+        <button onClick={handleBuy} className={styles.buy_btn}>Buy</button>
+      </div>
+    </div>
+  );
 }
-
