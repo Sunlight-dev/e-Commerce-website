0436f29df539f0e128c8c8227d5ee90912cdf10e
Sunlight-dev <rameshgiri0822@gmail.com>
2023-09-04 20:35:57 -0500

    adecuación de ruta getProductReviews con handler y controller a nuevo modelo de order

--- server/src/handlers/reviewsHandlers/getProductReviewsHandler.js
+++ server/src/handlers/reviewsHandlers/getProductReviewsHandler.js
@@ -2,8 +2,9 @@ const getProductReviewsController = require('../../controllers/reviewsController
 
 const getProductReviewsHandler = async(req,res)=>{
     const {productId} = req.params;
+    const {userId} = req.query;
     try {
-        const result = await getProductReviewsController(productId);
+        const result = await getProductReviewsController(productId,userId);
         res.status(200).json(result);    
     } 
     catch (error) {
