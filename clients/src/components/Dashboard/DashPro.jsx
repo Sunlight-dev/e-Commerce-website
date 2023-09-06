67e60d812a245c00bb033b99376fa6a4832956cc
Sunlight-dev <rameshgiri0822@gmail.com>
2023-09-06 03:14:21 -0300

    fix dashboard modular

--- clients/src/components/Dashboard/DashPro.jsx
+++ clients/src/components/Dashboard/DashPro.jsx
@@ -1,92 +1,15 @@
 import React from 'react'
 import Styles from './DashPro.module.css';
 import Form from '../CreateProduct/Form';
+import UserBuys from './UserBuys';
 
 
 export default function DashPro() {
   return (
     <div className={Styles.view}>
       <h1>Admin dashboard</h1>
-      <div className={Styles.separador}>
-          <div className={Styles.buys_wrapper}>
-              <h2>My Orders</h2>
-              <div className={Styles.order}>
-                
-              <div className={Styles.items}>
-                  <p>id</p>
-                  <p>date</p>
-                  <p>Product</p>
-                  <p>State</p>
-                  <p>arrives</p>
-                </div>
-                <div className={Styles.datos}>
-                  <p>#123</p>
-                  <p>23/07/2023</p>
-                  <p>Cacatúa</p>
-                  <p> Payd </p>
-                  <p> 29/07/2023</p>
-
-                </div>
-              </div>
-              <div className={Styles.order}>
-                
-              <div className={Styles.items}>
-                  <p>id</p>
-                  <p>date</p>
-                  <p>Product</p>
-                  <p>State</p>
-                  <p>arrives</p>
-                </div>
-                <div className={Styles.datos}>
-                  <p>#123</p>
-                  <p>23/07/2023</p>
-                  <p>Keyboard Bluetooth</p>
-                  <p> Payd </p>
-                  <p> 29/07/2023</p>
-
-                </div>
-              </div><div className={Styles.order}>
-                
-                <div className={Styles.items}>
-                    <p>id</p>
-                    <p>date</p>
-                    <p>Product</p>
-                    <p>State</p>
-                    <p>arrives</p>
-                  </div>
-                  <div className={Styles.datos}>
-                    <p>#123</p>
-                    <p>23/07/2023</p>
-                    <p>Pillow 2.0</p>
-                    <p> Payd </p>
-                    <p> 29/07/2023</p>
-  
-                  </div>
-                </div><div className={Styles.order}>
-                
-                <div className={Styles.items}>
-                    <p>id</p>
-                    <p>date</p>
-                    <p>Product</p>
-                    <p>State</p>
-                    <p>arrives</p>
-                  </div>
-                  <div className={Styles.datos}>
-                    <p>#123</p>
-                    <p>23/07/2023</p>
-                    <p>Producto</p>
-                    <p> Payd </p>
-                    <p> 29/07/2023</p>
-  
-                  </div>
-                </div>
-              
-           </div>
-          <div className={Styles.sells_wrapper}>
-              <h2>Sells</h2>
-
-      </div>
-      </div>
+      
+      <UserBuys/>
       <div className={Styles.form_wrapper}>
         <div className={Styles.form_left}>
            <h3>Sell your products in our platform <h4>D'Lujo</h4></h3>
@@ -100,3 +23,4 @@ export default function DashPro() {
     </div>
   )
 }
+
