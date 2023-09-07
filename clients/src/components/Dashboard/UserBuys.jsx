c277e47e52de54a276e251eb9df77f7a2e48c3bc
Sunlight-dev <rameshgiri0822@gmail.com>
2023-09-06 22:29:44 -0300

    Feat dashboard datos order from ddbb ordersRedux

--- clients/src/components/Dashboard/UserBuys.jsx
+++ clients/src/components/Dashboard/UserBuys.jsx
@@ -1,88 +1,58 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Styles from './UserBuys.module.css'
+import { useDispatch, useSelector } from 'react-redux'
+import { getOrders } from '../../Redux/actions/actions'
 
 export default function UserBuys() {
+  let dispatch = useDispatch()
+  useEffect(()=>{
+    dispatch(getOrders())
+  },[])
+  let ordersRedux = useSelector(state => state.ordersRedux)
   return (
     <div className={Styles.separador}>
           <div className={Styles.buys_wrapper}>
               <h2>My Orders</h2>
-              <div className={Styles.order}>
-                
-              <div className={Styles.items}>
-                  <p>id</p>
-                  <p>date</p>
-                  <p>Product</p>
-                  <p>State</p>
-                  <p>arrives</p>
-                </div>
-                <div className={Styles.datos}>
-                  <p>#123</p>
-                  <p>23/07/2023</p>
-                  <p>Cacatúa</p>
-                  <p> Payd </p>
-                  <p> 29/07/2023</p>
 
-                </div>
-              </div>
-              <div className={Styles.order}>
-                
-              <div className={Styles.items}>
-                  <p>id</p>
-                  <p>date</p>
-                  <p>Product</p>
-                  <p>State</p>
-                  <p>arrives</p>
-                </div>
-                <div className={Styles.datos}>
-                  <p>#123</p>
-                  <p>23/07/2023</p>
-                  <p>Keyboard Bluetooth</p>
-                  <p> Payd </p>
-                  <p> 29/07/2023</p>
+              {ordersRedux[0] && (
+              ordersRedux.map((pdt, idx)=>(
+                <div className={Styles.order}>
+                    <div className={Styles.items}>
 
+                    </div>
+                    <div className={Styles.datos} key={idx} >
+                      <p> {`id: ${pdt.id}`} </p>
+                      <p>{`Ordered: ${pdt.orderDate}`} </p>
+                      {pdt.deliveredDate && (
+                      <p>{`Arrives ${pdt.deliveredDate}`} </p>
+                      )}
+                      <p>{`Status: ${pdt.status}`} </p>
+                    </div>
                 </div>
-              </div><div className={Styles.order}>
-                
-                <div className={Styles.items}>
-                    <p>id</p>
-                    <p>date</p>
-                    <p>Product</p>
-                    <p>State</p>
-                    <p>arrives</p>
-                  </div>
-                  <div className={Styles.datos}>
-                    <p>#123</p>
-                    <p>23/07/2023</p>
-                    <p>Pillow 2.0</p>
-                    <p> Payd </p>
-                    <p> 29/07/2023</p>
+
+              ))
+              ) }{ordersRedux[0] && (
+                ordersRedux.map((pdt, idx)=>(
+                  <div className={Styles.order}>
+                      <div className={Styles.items}>
   
+                      </div>
+                      <div className={Styles.datos} key={idx} >
+                        <p> {`id: ${pdt.id}`} </p>
+                        <p>{`Ordered: ${pdt.orderDate}`} </p>
+                        <p>{`Status: ${pdt.status}`} </p>
+                        <p>{`arrives ${pdt.deliveredDate}`} </p>
+                      </div>
                   </div>
-                </div><div className={Styles.order}>
-                
-                <div className={Styles.items}>
-                    <p>id</p>
-                    <p>date</p>
-                    <p>Product</p>
-                    <p>State</p>
-                    <p>arrives</p>
-                  </div>
-                  <div className={Styles.datos}>
-                    <p>#123</p>
-                    <p>23/07/2023</p>
-                    <p>Producto</p>
-                    <p> Payd </p>
-                    <p> 29/07/2023</p>
   
-                  </div>
-                </div>
-              
-           </div>
+                ))
+                ) }
+          </div>
           <div className={Styles.sells_wrapper}>
               <h2>Sells</h2>
               <p>No sells</p>
 
-      </div>
+          </div>
       </div>
   )
 }
