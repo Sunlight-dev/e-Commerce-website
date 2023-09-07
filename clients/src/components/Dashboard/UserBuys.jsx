b9836a6733b28f4e1a6d58ccbd9faede31b04601
Sunlight-dev <rameshgiri0822@gmail.com>
2023-09-06 22:43:03 -0300

    Feat dashboard sells&buys dashboard styles

--- clients/src/components/Dashboard/UserBuys.jsx
+++ clients/src/components/Dashboard/UserBuys.jsx
@@ -2,19 +2,21 @@ import React, { useEffect } from 'react'
 import Styles from './UserBuys.module.css'
 import { useDispatch, useSelector } from 'react-redux'
 import { getOrders } from '../../Redux/actions/actions'
+import { NavLink } from 'react-router-dom'
 
 export default function UserBuys() {
   let dispatch = useDispatch()
   useEffect(()=>{
     dispatch(getOrders())
   },[])
-  let ordersRedux = useSelector(state => state.ordersRedux)
+  let sells = null;
+  let ordersRedux = useSelector(state => state.ordersRedux);
   return (
     <div className={Styles.separador}>
           <div className={Styles.buys_wrapper}>
               <h2>My Orders</h2>
 
-              {ordersRedux[0] && (
+              {ordersRedux[0] ? (
               ordersRedux.map((pdt, idx)=>(
                 <div className={Styles.order}>
                     <div className={Styles.items}>
@@ -31,7 +33,13 @@ export default function UserBuys() {
                 </div>
 
               ))
-              ) }{ordersRedux[0] && (
+              ):(
+                <div className={Styles.no_buys}>
+                  <p>There are no orders in your account yet. <NavLink to="/products">Browse our products</NavLink> and start shopping!</p>                  
+                </div>
+
+              )
+              }{ordersRedux[0] && (
                 ordersRedux.map((pdt, idx)=>(
                   <div className={Styles.order}>
                       <div className={Styles.items}>
@@ -49,9 +57,12 @@ export default function UserBuys() {
                 ) }
           </div>
           <div className={Styles.sells_wrapper}>
-              <h2>Sells</h2>
-              <p>No sells</p>
-
+            <h2>Sells</h2>
+            {sells ? (null):(
+              <div className={Styles.no_sells}>
+              <p>You haven't made any sales yet. Why not start selling your products in D'Lujo now?</p>
+              </div>
+            )}
           </div>
       </div>
   )
