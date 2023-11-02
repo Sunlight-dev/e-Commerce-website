0882f93ddd1ef7ae7031bcd45e1eb4070faf07e2
Sunlight-dev <rameshgiri0822@gmail.com>
2023-11-02 20:58:40 -0300

    deploy ROUTES REVIEW FIXED -

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -2,8 +2,8 @@ const { Router } = require('express');
 const productsRouter = require('./productRoutes.js');
 const categoriesRouter = require('./categoriesRouter.js');
 const usersRouter = require('./usersRouter.js');
-const reviewsRouter = require('./reviewsRouter');
-const mercadoPagoRouter = require('./mercadoPagoRouter')
+const reviewsRouter = require('./reviewsRouter.js');
+const mercadoPagoRouter = require('./mercadoPagoRouter.js')
 
 const brandsRouter = require('./brandsRouter.js');
 const ordersRouter = require('./ordersRouter.js');
@@ -13,7 +13,7 @@ const router = Router();
 router.use('/products', productsRouter);
 router.use('/categories', categoriesRouter);
 router.use('/users', usersRouter);
-// router.use('/reviews', reviewsRouter);
+router.use('/reviews', reviewsRouter);
 router.use('/mercadopago', mercadoPagoRouter)
 
 router.use('/brands', brandsRouter);
--- server/src/routes/reviewsRouter.js
+++ server/src/routes/reviewsRouter.js
@@ -1,13 +1,13 @@
-// const {Router} = require('express');
-// const getProductReviewsHandler = require ('../handlers/reviewsHandlers/getProductReviewsHandler.js');
-// const postProductReviewByUserHandler = require ('../handlers/reviewsHandlers/postProductReviewByUserhandler.js');
-// const updateProductReviewByUserHandler = require ('../handlers/reviewsHandlers/updateProductReviewByUserHandler.js');
-// const deleteProductReviewByUserHandler = require ('../handlers/reviewsHandlers/deleteProductReviewByUserHandler.js');
-// const reviewRouter = Router();
+const {Router} = require('express');
+const getProductReviewsHandler = require ('../handlers/reviewsHandlers/getProductReviewsHandler.js');
+const postProductReviewByUserHandler = require ('../handlers/reviewsHandlers/postProductReviewByUserhandler.js');
+const updateProductReviewByUserHandler = require ('../handlers/reviewsHandlers/updateProductReviewByUserHandler.js');
+const deleteProductReviewByUserHandler = require ('../handlers/reviewsHandlers/deleteProductReviewByUserHandler.js');
+const reviewRouter = Router();
 
-// reviewRouter.get('/:productId',getProductReviewsHandler);
-// reviewRouter.post('/:orderId',postProductReviewByUserHandler);
-// reviewRouter.put('/:reviewId',updateProductReviewByUserHandler);
-// reviewRouter.delete('/:reviewId',deleteProductReviewByUserHandler);
+reviewRouter.get('/:productId',getProductReviewsHandler);
+reviewRouter.post('/:orderId',postProductReviewByUserHandler);
+reviewRouter.put('/:reviewId',updateProductReviewByUserHandler);
+reviewRouter.delete('/:reviewId',deleteProductReviewByUserHandler);
 
-// module.exports = reviewRouter;
+module.exports = reviewRouter;
