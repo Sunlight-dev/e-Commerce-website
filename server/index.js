98bfd38a89784c982fccb7cbcba24adf5abdb6e5
Sunlight-dev <rameshgiri0822@gmail.com>
2023-11-02 22:12:06 -0300

    CORS SERVER BACK GET PRODUCTS

--- server/index.js
+++ server/index.js
@@ -1,10 +1,16 @@
 const server = require('./src/app')
 const { conn } = require('./src/db')
 
+const getAllProductsApi = require('./src/handlers/productsHandlers/getAllProductsApi.js')
+const {
+  addCategories,
+} = require('./src/controllers/categoriesControllers/addCategoryController.js')
 
 const PORT = process.env.PORT || 3001
 
 conn.sync({ force: false }).then(() => {
+  addCategories()
+  getAllProductsApi()
   server.listen(PORT, () => {
     console.log(`%s listening at ${PORT}`) // eslint-disable-line no-console
   })
